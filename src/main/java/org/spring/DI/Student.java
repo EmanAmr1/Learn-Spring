4b03eabb1d1aa45946a5491620b9b52992a33c6f
EmanAmr1 <emanamr543@gmail.com>
2024-06-12 16:04:18 +0300

    new obj by Setter Injection in SPRING

--- src/main/java/org/spring/DI/Student.java
+++ src/main/java/org/spring/DI/Student.java
@@ -2,11 +2,22 @@ package org.spring.DI;
 
 public class Student {
 
+
+    private int id;
+
    private String StudentName;
 
     public void displayStudentInfo(){
-        System.out.println("Student name is : "+ StudentName);
+        System.out.println("Student name is : "+ StudentName +" and id "+id);
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getStudentName() {
