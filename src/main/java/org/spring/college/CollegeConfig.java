cb53e149e8339e751f89aec0ff2f43ef97d91ee6
EmanAmr1 <emanamr543@gmail.com>
2024-07-01 15:49:41 +0300

    no need to use bean.xml make configuration class

---
+++ src/main/java/org/spring/college/CollegeConfig.java
@@ -0,0 +1,9 @@
+package org.spring.college;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.spring.college")
+public class CollegeConfig {
+}
--- src/main/java/org/spring/college/CollegeMain.java
+++ src/main/java/org/spring/college/CollegeMain.java
@@ -2,12 +2,12 @@ package org.spring.college;
 
 import org.spring.School.Student;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CollegeMain {
     public static void main(String[] args) {
-        ApplicationContext context =new ClassPathXmlApplicationContext("beans.xml");
-        System.out.println("beans file is loaded ");
+        ApplicationContext context =new AnnotationConfigApplicationContext(CollegeConfig.class);
         College c =context.getBean("collegeClass",College.class);
        System.out.println("the college object created by spring is:" +c);
     }
