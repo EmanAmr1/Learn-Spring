da9fc348bc1b5d72fb0884778b4040953b5cb2a0
EmanAmr1 <emanamr543@gmail.com>
2024-07-01 11:23:32 +0300

    autowiring by constructor

--- src/main/java/org/spring/autowired/Human.java
+++ src/main/java/org/spring/autowired/Human.java
@@ -5,6 +5,10 @@ public class Human {
     private Heart heart;
 
 
+    public Human(Heart heart) {
+        this.heart = heart;
+    }
+
     public void setHeart(Heart heart) {
         this.heart = heart;
     }
