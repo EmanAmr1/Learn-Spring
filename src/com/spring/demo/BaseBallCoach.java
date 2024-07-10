5698e40d45ff53abbccadb6a1675a00eca222b3f
Eman Amr Ali Hamed <eamr@ntgclarity.com>
2024-07-10 09:47:07 +0300

    intit java project

---
+++ src/com/spring/demo/BaseBallCoach.java
@@ -0,0 +1,13 @@
+package com.spring.demo;
+
+public class BaseBallCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {	
+			return "spending 30 min in batting practice";
+				
+	}
+	
+	
+
+}
---
+++ src/com/spring/demo/MyApp.java
@@ -0,0 +1,10 @@
+package com.spring.demo;
+
+public class MyApp {
+	
+	public static void main (String[] args) {
+		Coach c = new TrackCoach();
+		System.out.println(c.getDailyWorkOut());
+	}
+
+}
