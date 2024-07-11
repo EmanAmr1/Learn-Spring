f842f302e1bd9c40725b4db6ee6ec60ca5e2e6ee
Eman Amr Ali Hamed <eamr@ntgclarity.com>
2024-07-11 12:54:46 +0300

    @Scope Annotation scope => prototype

--- src/annotations/TennisPlaying.java
+++ src/annotations/TennisPlaying.java
@@ -1,8 +1,10 @@
 package annotations;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisPlaying implements Play {
 
 	@Override
