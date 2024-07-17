8119698046094b8213b4473fdedad349c90fc7ae
Eman Amr <emanamr543@gmail.com>
2024-07-17 15:07:21 +0300

    @OneToOne Bi-Directional print the instructor and the instructor detail

---
+++ src/DBRelationships/GetInstractorDetailDemo.java
@@ -0,0 +1,45 @@
+package DBRelationships;
+
+public class GetInstractorDetailDemo {
+
+	
+	public static void main(String[] args) {
+		//create session factory
+		SessionFactory factory =new Configuration()
+				.configure("hibernate.cfg.xml");
+		         .addAnnotatedClass(Instractor.class);
+		         .addAnnotatedClass(InstractorDetail.class);
+		         .buildSessionFactory();
+		         
+		//create session
+		         Session session =factory.getCurrentSession();
+		         
+		         try {
+		  
+		        	 
+		        	 //start a transaction
+		        	 session.beginTransaction();
+		        	
+		        	 //get the instructor detail object 
+		        	 int theId =1;
+		        	 InstructorDetail tempInstructorDetail =session.get(InstructorDetail.class ,theId );
+		        	
+		        	 
+		        	//print the instructor detail
+		        	 System.out.println("Found InstructorDetail: "+ tempInstructor );
+		        	 
+		        	 //print the associated instructor
+		        	 System.out.println("the associated instructor "+tempInstructorDetail.getInstructor());
+		        	 
+		        	 
+		        	 //commit the transaction
+		        	 session.getTransaction().commit();
+		        	 System.out.println("Done ");
+		         }
+		         finally {
+		        	 factory.close();
+		         }
+	
+	
+	
+}
